3298464fe67c24264ee011d77ded916d483da3d2
eustrain <eustrain@outlook.com>
2024-11-24 12:29:34 +0800

    feat: define sender and receiver

---
+++ sr.go
@@ -0,0 +1,19 @@
+package sr
+
+type Receiver[T any] interface {
+	SetValue(T)
+	SetError(err error)
+	SetStoped()
+}
+
+type Sender[T any] interface {
+	Connected(Receiver[T]) OperationState
+}
+
+type Scheduler[T any] interface {
+	Schedule() Sender[T]
+}
+
+type OperationState interface {
+	Start()
+}
