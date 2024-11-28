a32de011d67926fbc7e42d478fc5cfc46d8dca91
eustrain <eustrain@outlook.com>
2024-11-28 22:43:01 +0800

    fix: let value

--- let_value.go
+++ let_value.go
@@ -1,28 +1,46 @@
 // sender factory: LetValue
 package sr
 
-type letValueSender[T any] struct {
-	tag SenderTag
-	f   func() T
+type letValueSender[T any, R any] struct {
+	s Sender[T]
+	f func(T) R
 }
 
-func LetValue[T any](f func() T, tag SenderTag) Sender[T] {
-	return letValueSender[T]{f: f, tag: tag}
+func LetValue[T any, R any](f func(T) R, s Sender[T]) Sender[R] {
+	return letValueSender[T, R]{f: f, s: s}
 }
 
-func (s letValueSender[T]) Connect(r Receiver[T]) OperationState {
-	return letValueSenderState[T]{f: s.f, r: r}
+func (s letValueSender[T, R]) Connect(r Receiver[R]) OperationState {
+	return letValueSenderState[T, R]{f: s.f, r: r}
 }
 
-func (s letValueSender[T]) Tag() SenderTag {
-	return s.tag
+func (s letValueSender[T, R]) Tag() SenderTag {
+	return s.s.Tag()
 }
 
-type letValueSenderState[T any] struct {
-	f func() T
-	r Receiver[T]
+type letValueSenderState[T any, R any] struct {
+	s Sender[T]
+	f func(T) R
+	r Receiver[R]
 }
 
-func (state letValueSenderState[T]) Start() {
-	state.r.SetValue(state.f())
+func (state letValueSenderState[T, R]) Start() {
+	state.s.Connect(letValueReceiver[T, R]{f: state.f, r: state.r}).Start()
+}
+
+type letValueReceiver[T any, R any] struct {
+	f func(T) R
+	r Receiver[R]
+}
+
+func (lvr letValueReceiver[T, R]) SetValue(v T) {
+	lvr.r.SetValue(lvr.f(v))
+}
+
+func (lvr letValueReceiver[T, R]) SetError(err error) {
+	lvr.r.SetError(err)
+}
+
+func (lvr letValueReceiver[T, R]) SetStoped() {
+	lvr.r.SetStoped()
 }
