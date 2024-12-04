723973030b9d6d77a9a8b49b4ef169f0a3b7e049
eustrain <eustrain@outlook.com>
2024-12-04 23:01:29 +0800

    feat: connect to channel

---
+++ src/connect_to_result_channel.go
@@ -0,0 +1,21 @@
+package src
+
+import "github.com/samber/mo"
+
+func ConnectToResultChannel[T any](s Sender[T], ch chan<- mo.Result[T]) OperationState {
+	return s.Connect(startOnChannelReceiver[T](ch))
+}
+
+type startOnChannelReceiver[T any] chan<- mo.Result[T]
+
+func (r startOnChannelReceiver[T]) SetValue(v T) {
+	r <- mo.Ok(v)
+}
+
+func (r startOnChannelReceiver[T]) SetError(err error) {
+	r <- mo.Err[T](err)
+}
+
+func (r startOnChannelReceiver[T]) SetStoped() {
+	close(r)
+}
