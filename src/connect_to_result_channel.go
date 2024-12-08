fc7e55f4d1d652b0ce4422b1fe2bc1b20fd17637
eustrain <eustrain@outlook.com>
2024-12-08 16:06:09 +0800

    fix: refine connect to channel

--- src/connect_to_result_channel.go
+++ src/connect_to_result_channel.go
@@ -2,20 +2,28 @@ package src
 
 import "github.com/samber/mo"
 
-func ConnectToResultChannel[T any](s Sender[T], ch chan<- mo.Result[T]) OperationState {
-	return s.Connect(startOnChannelReceiver[T](ch))
+func ConnectToResultChannel[T any](s Sender[T], ch chan<- mo.Result[T], onStoped func()) OperationState {
+	return s.Connect(startOnChannelReceiver[T]{
+		resultCh: ch,
+		onStoped: onStoped,
+	})
 }
 
-type startOnChannelReceiver[T any] chan<- mo.Result[T]
+type startOnChannelReceiver[T any] struct {
+	resultCh chan<- mo.Result[T]
+	onStoped func()
+}
 
 func (r startOnChannelReceiver[T]) SetValue(v T) {
-	r <- mo.Ok(v)
+	r.resultCh <- mo.Ok(v)
 }
 
 func (r startOnChannelReceiver[T]) SetError(err error) {
-	r <- mo.Err[T](err)
+	r.resultCh <- mo.Err[T](err)
 }
 
 func (r startOnChannelReceiver[T]) SetStoped() {
-	close(r)
+	if r.onStoped != nil {
+		r.onStoped()
+	}
 }
